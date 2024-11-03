e64e407edcb428af278babe56608db51cd1f81c3
Hyna <bylo@duck.com>
2024-11-03 23:14:38 +0100

    quete express : GET en détail

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -4,9 +4,24 @@ import database from "../../database.js";
 // Récupérer tous les films
 export const getMovies = async (req, res) => {
   try {
-    const [movies] = await database.query("SELECT * from movies");
+    let sql = "SELECT * from movies";
+    const sqlValues = [];
+    const color = req.query.color;
+    const max_duration = req.query.max_duration;
+    if (color != null) {
+      sql += " WHERE color = ?";
+      sqlValues.push(color);
+    }
+
+    if (max_duration != null) {
+      color ? (sql += " AND duration <= ?") : " WHERE duration <= ?";
+      sqlValues.push(max_duration);
+    }
+
+    const [movies] = await database.query(sql, sqlValues);
     res.status(200).json(movies);
   } catch (err) {
+    // console.error("Erreur lors de l'exécution de la requête:", err);
     res.status(500).send("Erreur lors de la récupération des films");
   }
 };
@@ -15,7 +30,10 @@ export const getMovies = async (req, res) => {
 export const getMovieById = async (req, res) => {
   const id = parseInt(req.params.id);
   try {
-    const [movie] = await database.query("SELECT * from movies WHERE `id` = ?", [id]);
+    const [movie] = await database.query(
+      "SELECT * from movies WHERE `id` = ?",
+      [id]
+    );
     if (movie.length > 0) {
       res.json(movie);
     } else {
@@ -29,9 +47,26 @@ export const getMovieById = async (req, res) => {
 // Récupérer tous les utilisateurs
 export const getUsers = async (req, res) => {
   try {
-    const [users] = await database.query("SELECT * FROM users");
+    let sql = "SELECT * FROM users";
+    const sqlValues = [];
+
+    // Récupérer les paramètres optionnels language et city s'ils existent
+    const language = req.query.language;
+    const city = req.query.city;
+
+    if (language != null) {
+      sql += " WHERE language = ?";
+      sqlValues.push(language);
+    }
+    if (city != null) {
+      sql += language ? " AND city = ?" : " WHERE city = ?";
+      sqlValues.push(city);
+    }
+
+    const [users] = await database.query(sql, sqlValues);
     res.status(200).json(users);
   } catch (err) {
+    console.error("Erreur lors de la récupération des utilisateurs:", err);
     res.status(500).send("Erreur lors de la récupération des utilisateurs");
   }
 };
@@ -40,13 +75,17 @@ export const getUsers = async (req, res) => {
 export const getUsersByID = async (req, res) => {
   const id = parseInt(req.params.id);
   try {
-    const [user] = await database.query("SELECT * from users WHERE `id` = ?", [id]);
+    const [user] = await database.query("SELECT * from users WHERE `id` = ?", [
+      id,
+    ]);
     if (user.length > 0) {
       res.json(user);
     } else {
       res.status(404).send("L'utilisateur recherché n'existe pas");
     }
   } catch (err) {
-    res.status(500).send("Erreur dans la tentative de récupération de l'utilisateur");
+    res
+      .status(500)
+      .send("Erreur dans la tentative de récupération de l'utilisateur");
   }
 };
